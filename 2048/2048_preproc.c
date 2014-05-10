2df4f449f535bc9ceebea37276f7a98fd4ba67f1
Jean-Yves Lamoureux <jylam@lnxscene.org>
2014-05-10 20:39:14 +0200

    * 2048

---
+++ 2048/2048_preproc.c
@@ -0,0 +1,7 @@
+#include "/home/jylam/code/oric/public/pc/tools/osdk/main/Osdk/_final_/macro/MACROS.H"
+/* 16bit code V1.29 by F.Frances */
+_main
+	ENTER(0,8)
+	ASGNW_CA(42,(fp),6)
+	ADDW_ICI((fp),6,1,(fp),6)
+	LEAVE
