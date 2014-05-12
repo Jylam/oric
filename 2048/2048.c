01f76471e3467059461ce3c488e194e599363b4d
jylam <jylam@lnxscene.org>
2014-05-12 19:41:29 +0200

    Board

--- 2048/2048.c
+++ 2048/2048.c
@@ -1,7 +1,67 @@
-int main(int argc, char *argv[]) {
-    int i;
-    char *text = (char*)0xbb80;
-    for(i=0; i<10; i++) {
-        text[i] = 65;
+#include <sys/graphics.h>
+
+char *screen = (char*)0xbb80;
+char board[4*4] = {0};
+
+
+void draw_board(void) {
+    int x = 0;
+    int y = 0;
+
+    for(x=0; x<40; x++) {
+        screen[x+y*40] = '-';
     }
+
+    y = 40;
+    for(x=0; x<40; x++) {
+        screen[x+y*40] = '-';
+    }
+
+    for(x = 0; x < 40; x+=10) {
+        for(y=0; y<40; y++) {
+            screen[x+y*40] = 'l';
+        }
+    }
+
+    for(y = 0; y < 29; y+=7) {
+        for(x=0; x<40; x++) {
+            screen[x+y*40] = '-';
+        }
+    }
+
+
+    for(y = 0; y < 4; y++) {
+        for(x = 0; x < 4; x++) {
+            char str[5];
+            int i = 0;
+            for(i=0; i < strlen(str); i++) {
+
+            }
+        }
+    }
+
+
+
 }
+
+void clear_screen(void) {
+    memset(screen, ' ', 40*28);
+}
+
+
+void game(void) {
+    draw_board();
+}
+
+int main(int argc, char *argv[]) {
+    curset(0, 0, MODE_NONE);
+    clear_screen();
+    draw_board();
+
+    while(1) {
+        game();
+    }
+
+}
+
+
