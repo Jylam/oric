7c896075935acf7201ece22945785219b8b41486
Jean-Yves Lamoureux <jylam@lnxscene.org>
2014-05-22 23:32:37 +0200

    Scanline moves

--- vectorballs/main.c
+++ vectorballs/main.c
@@ -7,18 +7,27 @@ unsigned char *screen_text = (unsigned char*)0xbf68;
 extern void draw_sprite(unsigned char x, unsigned char y, unsigned char color);
 
 int main(int argc, char *argv[]) {
-    unsigned char x, y;
+    unsigned char x=0, y=0, oldx=0, oldy=0;
+    char tx=1, ty=1;
     int t;
 
     hires();
     setflags(getflags()&~(CURSOR|SCREEN)); // Disable cursor and scrolling
 
     while(1) {
-        for(x = 0; x < 32; x++) {
-            for(t=0; t < 1000; t++);
-                draw_sprite(x, x, 0b01100001);
+        if(x==0)  tx =  1;
+        if(x==32) tx = -1;
+        if(y==0)   ty =  5;
+        if(y==200) ty = -5;
+        x+=tx;
+        y+=ty;
 
-        }
+        for(t=0; t < 1000; t++);
+        // Clear the old one
+        draw_sprite(oldx, oldy, 0b01000000);
+        draw_sprite(x, y, 0b01100001);
+        oldx = x;
+        oldy = y;
     }
 }
 
