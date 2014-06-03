de7c6c1510ade42ba93c09240600b29f4004bfa9
Jean-Yves Lamoureux <jylam@lnxscene.org>
2014-06-03 23:02:08 +0200

    Sin/cos tables

--- vectorballs/pc_test/2d.c
+++ vectorballs/pc_test/2d.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+
 #define FP_W 8
 
 #define FIXED unsigned short
@@ -10,7 +11,7 @@
 #define FP(a) ((a)<<FP_W)
 #define INT(a) (((FIXED)a)>>FP_W)
 #define FLOAT_TO_FP(a) ((a)*(float)FP_MASK)
-#define FP_TO_FLOAT(a) ((a)/(float)FP_MASK)
+#define FP_TO_FLOAT(a) (((signed short)a)/(float)FP_MASK)
 
 #define FP_ADD(a, b) ((a)+(b))
 #define FP_SUB(a, b) ((a)-(b))
@@ -19,42 +20,31 @@
 
 #define FP_DEC(a)(((FIXED)(a & FP_MASK)<<FP_W)/(1<<FP_W))
 
+#include "tables.h"
 
 
-void rotateX(FIXED x, FIXED y, FIXED angle) {
-    FIXED x2;
-    FIXED y2;
-    FIXED s;
-    // x' = x*cos(theta) - y*sin(theta)
-    // y' = x*sin(theta) + y*cos(theta)
-//    x2 = FP_MUL(x, FLOAT_TO_FP(cos(FP_TO_FLOAT(angle)*M_PI/180.0f))) - FP_MUL(y, FLOAT_TO_FP(sin(FP_TO_FLOAT(angle)*M_PI/180.0f)));
-//    y2 = FP_MUL(x, FLOAT_TO_FP(sin(FP_TO_FLOAT(angle)*M_PI/180.0f))) + FP_MUL(y, FLOAT_TO_FP(cos(FP_TO_FLOAT(angle)*M_PI/180.0f)));
+void rotateX(FIXED x, FIXED y, FIXED cosa, FIXED sina) {
+        float x2, y2;
 
+        x2 = ((x*cosa)>>FP_W)-((y*sina)>>FP_W); // Result is wrong, cosa is fixed point and it shouldn't
+        y2 = ((x*sina)>>FP_W)+((y*cosa)>>FP_W);
 
-    printf("Angle %f, x %f, y %f\n", FP_TO_FLOAT(angle), FP_TO_FLOAT(x2), FP_TO_FLOAT(y2));
+        printf("%f  %f  ->  %f  %f\n", FP_TO_FLOAT(x), FP_TO_FLOAT(y), FP_TO_FLOAT(x2), FP_TO_FLOAT(y2));
 
-#if 0
-    printf("Angle %f (rad %f)  %f %f   sin %f -> %04x\n",
-        FP_TO_FLOAT(angle),
-        FP_TO_FLOAT(angle)*M_PI/180.0f,
-        FP_TO_FLOAT(x2),
-        FP_TO_FLOAT(y2),
-        sin(FP_TO_FLOAT(angle)*M_PI/180.0f),
-        FLOAT_TO_FP(sin(FP_TO_FLOAT(angle)*M_PI/180.0f)*100.0f));
-    s = FLOAT_TO_FP(sin(FP_TO_FLOAT(angle)*M_PI/180.0f));
-    printf("Angle %f, Sin : %f      FP : %f\n", FP_TO_FLOAT(angle), sin(FP_TO_FLOAT(angle)*M_PI/180.0f), FP_TO_FLOAT(s));
-#endif
 }
 
 void test_fp(void) {
-    FIXED x = FP(10), x2;
-    FIXED y = FP(0), y2;
-    FIXED a = FP(0);
+    FIXED x = FP(10);
+    FIXED y = FP(0);
+    FIXED x2, y2;
+    float angle;
+    float a;
+    int i = 0;
 
-    while(FP_TO_FLOAT(a)<=126.f) {
-        rotateX(x, y, a);
-        a = FP_ADD(a, FP(10));
-    }
+    for(i=0; i<256; i++) {
+        rotateX(x, y, cosa88[i], sina88[i]);
+
+       }
 
 }
 
@@ -66,27 +56,34 @@ void gen_tables(void) {
     float a;
     int i = 0;
 
+    printf("FIXED cosa88[] = {\n");
     for(i=0; i<256; i++) {
-        FIXED cosa, sina;
+        FIXED cosa;
         angle+=(360.0f/256.0f);
-
-        a = angle*M_PI/180.0f;
+        float a = (angle*M_PI/180.0f);
         cosa = FLOAT_TO_FP(cos(a));
+
+        printf("0x%04x, ", cosa);
+       }
+    printf("\n};\n");
+
+    printf("FIXED sina88[] = {\n");
+    for(i=0; i<256; i++) {
+        FIXED sina;
+        angle+=(360.0f/256.0f);
+        float a = (angle*M_PI/180.0f);
         sina = FLOAT_TO_FP(sin(a));
 
-        x2 = FP_MUL(x, cosa) - FP_MUL(y, sina);
-        y2 = FP_MUL(x, sina) + FP_MUL(y, cosa);
-
-        printf("%d: Angle %f (FP %f, should be %f),\tx = %f,\ty = %f\n", i, angle, FP_TO_FLOAT(a), angle*M_PI/180.0f,FP_TO_FLOAT((signed short)x2), FP_TO_FLOAT((signed short)y2));
-    }
-
+        printf("0x%04x, ", sina);
+       }
+    printf("\n};\n");
 }
 
 int main(int argc, char *argv[]) {
 
-    //    test_fp();
+    test_fp();
 
-    gen_tables();
+//    gen_tables();
 
 }
 
