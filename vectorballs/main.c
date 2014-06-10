11b456aafb5cbd196d266ae43f8f73748155bf62
Jean-Yves Lamoureux <jylam@lnxscene.org>
2014-06-10 22:48:18 +0200

    Rotating sprite

--- vectorballs/main.c
+++ vectorballs/main.c
@@ -11,13 +11,8 @@
 #define FIXED int
 
 typedef struct {
-    float x, y, z;
-} vec3;
-
-typedef struct {
-    vec3 pos;
-    unsigned char x, y, oldx, oldy;
-    char tx, ty;
+    FIXED x, y, z;
+    FIXED oldx, oldy, oldz;
 } sprite;
 
 extern void VSync(void);
@@ -60,18 +55,23 @@ void rotateX(FIXED x, FIXED y, FIXED cosa, FIXED sina, FIXED *x2, FIXED *y2) {
 
 
 void test_fp(void) {
-    FIXED x = FP(128);
+    FIXED x = FP(64);
     FIXED y = FP(0);
-    unsigned int i = 0;
+    unsigned char i = 0;
     int r = 2;
     static FIXED x2, y2;
+    FIXED oldx, oldy;
 
     hires();
-    for(i=0; i<255; i++) {
+    for(i=0; ; i++) {
         rotateX(x, y, cosa88[i], sina88[i], &x2, &y2);
     //    printf("%d: %x %x\n", i, x2, y2);
-        curset(x2+100, y2+100,3);
-        circle(r,2);
+    //    curset(x2+100, y2+100,3);
+      //  circle(r,2);
+        clear_sprite(oldx, oldy);
+        oldx = (x2/6)+20;
+        oldy = y2+100;
+        draw_sprite(oldx, oldy);
     }
 
 
@@ -103,42 +103,35 @@ void set_colors(void) {
 int main(int argc, char *argv[]) {
     unsigned char y;
     FIXED t=0xa000;
-    char tx=1, ty=1;
     int s = 0;
     FIXED f;
     unsigned char angle = 0;
     static FIXED x2, y2;
 
-#if 0
-    test_fp();
-while(1);
-#endif
     for(y=0 ; y<200; y++) {
         table_y[y] = t;
         t+=40;
     }
+#if 1
+    test_fp();
+while(1);
+#endif
 
     y = 0;
     for(s=0;s<NB_SPRITES;s++) {
         sprites[s].x = sprites[s].y = sprites[s].oldx = sprites[s].oldy = 2;
-        sprites[s].x = FP(64);
-        sprites[s].y = FP(64);
-        sprites[s].tx = (rand()&0x02)-1;
-        if(sprites[s].tx == 0) sprites[s].tx = 4;
-        sprites[s].ty = (rand()&0x03)-1;
-        if(sprites[s].ty == 0) sprites[s].ty = 4;
-
+        sprites[s].x = FP(32);
+        sprites[s].y = FP(0);
     }
 
-    hires();
-    setflags(getflags()&~(CURSOR|SCREEN)); // Disable cursor and scrolling
+//    hires();
+  //  setflags(getflags()&~(CURSOR|SCREEN)); // Disable cursor and scrolling
 
-    set_colors();
+//    set_colors();
     IrqOff();
 
     while(1) {
         // clear the old ones
-        VSync();
         for(s=0;s<NB_SPRITES;s++) {
             clear_sprite(sprites[s].oldx, sprites[s].oldy);
         }
@@ -149,15 +142,18 @@ while(1);
 //y' = y
 
             rotateX(sprites[s].x, sprites[s].y, cosa88[angle], sina88[angle], &x2, &y2);
+            printf("%x %x %x\n", angle, x2, y2);
             sprites[s].x = x2+100;
             sprites[s].y = y2+100;
-            angle++;
 
             sprites[s].oldx = sprites[s].x;
             sprites[s].oldy = sprites[s].y;
 
             draw_sprite(sprites[s].x, sprites[s].y);
         }
+            angle++;
+            angle=angle&0x0F;
+            VSync();
     }
 }
 
