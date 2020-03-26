b67b7e0d6e9778a5517852d8fba8e681e9db91d8
Jylam <jylam@lnxscene.org>
2020-03-26 21:32:04 +0100

    Shitty hires line

--- asteroric/main.c
+++ asteroric/main.c
@@ -17,12 +17,37 @@ void set_colors(void) {
     screen_text[81] = A_BGBLUE;
 }
 
+int abs(int v) {
+    if(v<0) return -v;
+    return v;
+}
+
+void line(int x0, int y0, int x1, int y1) {
+
+    int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
+    int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1;
+    int err = (dx>dy ? dx : -dy)/2, e2;
+
+    for(;;){
+//        setPixel(x0,y0);
+        screen[(y0*40)+x0] = 0x20;
+        if (x0==x1 && y0==y1) break;
+        e2 = err;
+        if (e2 >-dx) { err -= dy; x0 += sx; }
+        if (e2 < dy) { err += dx; y0 += sy; }
+    }
+}
+
+
 void main()
 {
     hires();
     set_colors();
 
-    screen[(100*40)+2] = 0x20; // 1px
-    screen[(101*40)+2] = 0x7E; // 6px
+    //screen[(100*40)+2] = 0x20; // 1px
+    //screen[(101*40)+2] = 0x7E; // 6px
+
+    line(10, 10, 35, 100);
+
 
 }
