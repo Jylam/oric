fecb3e80254ccd5b916aff15f85f1316f8b5abc9
Jylam <jylam@lnxscene.org>
2020-03-26 21:22:55 +0100

    hires and pixels

--- asteroric/main.c
+++ asteroric/main.c
@@ -8,22 +8,12 @@ volatile int table_y[200];
 
 void set_colors(void) {
     int y;
-    unsigned char mask = 0;
     for(y=0; y<200; y++) {
         screen[(y*40)+0] = A_FWYELLOW;
-        mask++;
-        if((y&mask)>64) {
-            screen[(y*40)+1] = A_BGBLUE;
-        } else {
-            screen[(y*40)+1] = A_BGBLACK;
-        }
+        screen[(y*40)+1] = A_BGBLACK;
     }
 
-    screen_text[0] = A_BGBLACK;
-    screen_text[1] = A_BGBLUE;
-    screen_text[40] = A_BGBLACK;
-    screen_text[41] = A_BGBLUE;
-    screen_text[80] = A_BGBLACK;
+    screen_text[80] = A_FWWHITE;
     screen_text[81] = A_BGBLUE;
 }
 
@@ -31,6 +21,8 @@ void main()
 {
     hires();
     set_colors();
-    printf("Hello World lol\n");
+
+    screen[(100*40)+2] = 0x20; // 1px
+    screen[(101*40)+2] = 0x7E; // 6px
 
 }
