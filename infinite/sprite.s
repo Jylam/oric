cbdf8ba296141bbbb8bdc0c691ea519d7207ce9b
jylam <jylam@lnxscene.org>
2020-04-15 08:50:33 +0200

    Revert "Spliced a bunch o'tables (h/l), put in ZP"
    This reverts commit 1fb5ed211252517a0e141991df24814c75e3b6bf.

--- infinite/sprite.s
+++ infinite/sprite.s
@@ -6,18 +6,11 @@ tmpsa           .dsb 1
 sexel_offset    .dsb 1
 pixel           .dsb 1
 sy              .dsb 1
-
-y_offset        .dsb 2
 _cur_buffer_ptr .dsb 2
+y_offset        .dsb 2
 sprite          .dsb 2
 sprite_alpha    .dsb 2
 screen_ptr      .dsb 2
-
-_table_pixel_value .dsb 6
-_sprite_ptrsLOW .dsb 6
-_sprite_ptrsHIGH .dsb 6
-_sprite_alpha_ptrsLOW .dsb 6
-_sprite_alpha_ptrsHIGH .dsb 6
 _pdbg           .dsb 2
 
 .text
@@ -43,21 +36,24 @@ sta pixel
 lda _px             ;; 34 OK
 sec
 sbc pixel
+asl            ;; sprite_ptrs holds 16bit values
 sta pixel
 tay
 
 ;; u8  *sprite = (u8*)sprite_ptrs[pixel]; // 16bits pointer to u8*
-lda _sprite_ptrsLOW, y
+lda _sprite_ptrs, y
 sta sprite
-lda _sprite_ptrsHIGH, y
+iny
+lda _sprite_ptrs, y
 sta sprite+1           ;; 0768 OK
 
 
 ;; u8  *sprite_alpha = (u8*)sprite_alpha_ptrs[pixel];
 ldy pixel
-lda _sprite_alpha_ptrsLOW, y
+lda _sprite_alpha_ptrs, y
 sta sprite_alpha
-lda _sprite_alpha_ptrsHIGH, y
+iny
+lda _sprite_alpha_ptrs, y
 sta sprite_alpha+1          ;; 0917 OK
 
 
