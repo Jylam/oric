6e7d2c40c821fef0651da175fcc0063858d5f7a9
jylam <jylam@lnxscene.org>
2020-04-16 09:16:58 +0200

    Reflection.

---
+++ infinite/copy.s
@@ -0,0 +1,126 @@
+_copy_buffer
+ldy #0
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+iny
+lda (_cur_buffer_ptr), y
+sta (_screen_cpy_ptr), y
+
+rts
