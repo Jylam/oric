225e3b3991c364a4ae93c44c4639fe625db35117
jylam <jylam@lnxscene.org>
2020-04-26 10:50:37 +0200

    Checkerboard

--- parallax/gen_checkerboard.py
+++ parallax/gen_checkerboard.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 import pygame
+import time
 pygame.init()
 
 screen = pygame.display.set_mode([216, 100])
@@ -13,19 +14,26 @@ while running:
             running = False
 
     board = pygame.Surface((216, 216))
-    screen.fill((255, 255, 255))
+    screen.fill((255, 0, 255))
 
     size = 27
-    cnt = 0
-    for y in range(0, 8):
-        cnt=cnt+1
-        for x in range(0, 8):
-            color = (0,0,0)
-            if cnt&1:
-                color = (255, 0, 0)
-            pygame.draw.rect(board, color, (x*size, y*size, size, size))
-            cnt = cnt+1
-    screen.blit(board, (0, 0), (0,0,216,10))
-    pygame.display.flip()
+    xoffset = 0;
+    while True:
+        cnt = 0
+        for y in range(0, 8):
+            cnt=cnt+1
+            for x in range(-8, 8):
+                color = (0,0,0)
+                if cnt&1:
+                    color = (255, 255, 255)
+                pygame.draw.rect(board, color, (x*size+xoffset, y*size, size, size))
+                cnt = cnt+1
 
+        for i in range(0, 100):
+            t = pygame.transform.scale(board, (int(216*(i/100.0)), 100))
+            screen.blit(t, (0, i), (0,i,216,1))
+    #    pygame.image.save(screen, "checkerboard.png")
+        pygame.display.flip()
+        time.sleep(.1)
+        xoffset+=1
 pygame.quit()
