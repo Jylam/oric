29c6360e9ad27d3e9653ccd6aa0cfe0eb304551f
jylam <jylam@lnxscene.org>
2020-05-03 08:51:33 +0200

    Disk test

--- disk/main.c
+++ disk/main.c
@@ -13,8 +13,19 @@ u8 *screen_text = (u8*)0xbf68;
 
 void main()
 {
-    IrqOff();
-
-    printf("WHOO\n");
+    u8 FDC_command = *(u8*)0x310;
+    u8 FDC_track   = *(u8*)0x311;
+    u8 FDC_sector  = *(u8*)0x312;
+    u8 FDC_data    = *(u8*)0x313;
+    u8 FDC_ready   = *(u8*)0x318;
+    //IrqOff();
+    printf("Infos:\n");
+    while(1) {
+            printf("Command %x\n", FDC_command);
+            printf("Track   %x\n", FDC_track);
+            printf("Sector  %x\n", FDC_sector);
+            printf("Data    %x\n", FDC_data);
+            printf("RDY     %x\n", FDC_ready);
+    }
     while(1);
 }
