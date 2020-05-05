b984e5b7659ef70af53aad2caa81a83bfba2c9fa
Jylam <jylam@lnxscene.org>
2020-05-05 19:33:35 +0200

    pointers

--- disk/fdc.s
+++ disk/fdc.s
@@ -1,8 +1,8 @@
-_FDC_command .dsb $310
-.dsb _FDC_track   $311
-_FDC_sector  .dsb $312
-_FDC_data    .dsb $313
-_FDC_ready   .dsb $318
+_FDC_command = $310
+_FDC_track   = $311
+_FDC_sector  = $312
+_FDC_data    = $313
+_FDC_ready   = $318
 
 
 _fdc_setup
